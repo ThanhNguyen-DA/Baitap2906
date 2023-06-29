524ba7afab9a9296525f9f64084cb81c375fcae2
Nguyễn Phùng Thành <106498219+ThanhNguyen-DA@users.noreply.github.com>
2023-06-29 15:47:12 +0700

    latest files

---
+++ index.php
@@ -0,0 +1,422 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="./style.css" />
+  </head>
+  <body style="background: linear-gradient(to right, rgb(222, 248, 242), #ffffff)">
+    <!-- header -->
+    <header id="header"  style="background: linear-gradient(to right, rgb(222, 248, 242), #ffffff)">
+      <div class="header-top">
+        <!-- top head -->
+        <div class="logo">
+          <p>Biccas</p>
+        </div>
+        <div class="menu">
+          <!--  menu -->
+          <nav class="main-menu">
+            <ul>
+              <li class="main-menu-item main-menu-item-active"><a href="">Home</a></li>
+              <li class="main-menu-item">
+                <a href="">Product</a>
+              </li>
+              <li class="main-menu-item"><a href="">FAQ</a></li>
+              <li class="main-menu-item"><a href="">Blog</a></li>
+              <li class="main-menu-item"><a href="">About Us</a></li>
+            </ul>
+          </nav>
+
+          <!-- button -->
+
+          <div class="header-button-group">
+            <button class="btn btn-danger">Login</button>
+            <button class="btn btn-info">Sign up</button>
+          </div>
+        </div>
+      </div>
+      <!-- bot head -->
+      <div class="header-bottom">
+        <!-- left -->
+        <div class="banner-description">
+          <p class="banner-description-title">
+           We're here to Increase your Productivity
+          </p>
+          <div class="banner-description-break"></div>
+
+          <p class="banner-description-content">
+            Let make your wwork more organize and easily using the Taskio Dashboard with many of the latest featuresin managing work everyday
+          </p>
+          <div class="banner-description-btn">
+            <button class="btn btn-warning btn-round">Try Free Trial</button>
+            <button class="btn">View Demo</button>
+          </div>
+        </div>
+
+        <!-- right -->
+
+        <div class="banner-image-wrap">
+          <img class="banner-image-wrap-img" src="./images/person.png" alt="Header bottom right img" />
+
+          <img class="credit-card" src="./images/card.png" alt="Credit card" />
+          <form action="">
+            <div class="form-group">
+              <p class="text-grey">Enter amount</p>
+              <input class="form-control-input" type="text" value="$450" />
+              <button class="btn btn-info btn-round btn-sm">Send</button>
+            </div>
+          </form>
+          <div class="form-group">
+            <p class="text-grey">Total Income</p>
+            <p>$250.00</p>
+            <img src="" alt="icon" />
+          </div>
+        </div>
+      </div>
+    </header>
+    <!-- main -->
+    <main id="main" class="main">
+      <!-- Part 1 -->
+      <section id="analyst">
+        <h2 class="main-text">More than 25,000 team use Collabs</h2>
+
+        <div class="company">
+          <div class="company-icon">
+            <img class="campany-icon-img" src="./images/unsplash.png" alt="Company Icon" />
+            <p>Unsplash</p>
+          </div>
+          <div class="company-icon">
+            <img class="campany-icon-img" src="./images/notion.png" alt="Company Icon" />
+            <p>Notion</p>
+          </div>
+          <div class="company-icon">
+            <img class="campany-icon-img" src="./images/intercom.png" alt="Company Icon" />
+            <p>INTERCOM</p>
+          </div>
+          <div class="company-icon">
+            <img class="campany-icon-img" src="./images/descript.png" alt="Company Icon" />
+            <p>descript</p>
+          </div>
+          <div class="company-icon">
+            <img class="campany-icon-img" src="./images/grammarly.png" alt="Company Icon" />
+            <p>grammarly</p>
+          </div>
+        </div>
+
+        <div class="introduce">
+          <div class="introduce-text">
+            <p style="font-size: 40px;">How we support our pratner all over the world</p>
+            <p>
+              SaaS become a common delivery model for many business applicatin, including office software, messaging software, payroll processing software, DBMS software, management software
+            </p>
+
+            <div class="rating">
+              <!-- rating -->
+              <div>
+                <img class="star" src="./images/star.png" alt="star icon" />
+                <p>4.9/5 rating</p>
+                <p>Databricks</p>
+              </div>
+              <div>
+                <img class="star" src="./images/star.png" alt="star icon" />
+                <p>4.9/5 rating</p>
+                <p>Databricks</p>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <div class="list">
+              <div class="list-push">
+                <img class="star" src="./images/analyst.png" alt="star icon" />
+              </div>
+              <div>
+                <h3>Pushlishing</h3>
+                <p>
+                  Plan, collaborate, and publishing your content that drivees meaningful engagement and growth for your barnd
+                </p>
+              </div>
+            </div>
+
+            <div>
+              <div>
+                <img class="star" src="./images/analyst.png" alt="star icon" />
+              </div>
+              <div>
+                <h3>Analytics</h3>
+                <p>
+                  Analyze your performance and create goegeous report
+                </p>
+              </div>
+            </div>
+
+            <div>
+              <div>
+                <img class="star" src="./images/analyst.png" alt="star icon" />
+              </div>
+              <div>
+                <h3>Engagement</h3>
+                <p>
+                  Quiuckly navigate you anda engage with your adience
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <!-- Part 2 -->
+      <section id="feature">
+        <div class="feature">
+          <div class="feature-text">
+            <h1>
+              Our Features you cab het
+            </h1>
+          </div>
+          <div class="feature-text">
+            <p>
+              We offer a variety of interesting features that you can help increase you productivity at work and manage your project esaly
+            </p>
+          </div>
+          <div class="feature-text">
+            <button class="btn btn-info btn-round">Get Started</button>
+          </div>
+        </div>
+        <div class="feature-content">
+          <div  class="feature-content-list">
+            <div>
+              <img class="feature-img" src="./images/mac.png" alt="Logo" />
+            </div>
+            <h1>Colloboration Team</h1>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+            </p>
+          </div>
+          <div class="feature-content-list">
+            <div>
+              <img class="feature-img" src="./images/mac.png" alt="Logo" />
+            </div>
+            <h1>Cloud Storage</h1>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+            </p>
+          </div>
+          <div  class="feature-content-list">
+            <div>
+              <img class="feature-img" src="./images/mac.png" alt="Logo" />
+            </div>
+            <h1>Daily Analytics</h1>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <!-- Part 3 -->
+      <section class="benifit" id="benifit">
+        <div class="benifit-content">
+          <h1 style="padding: 0 50px 20px; font-size: 32px;">What benifit will you get</h1>
+          <ul>
+            <li class="benifit-text">Free Consulting With Experet Saving Money</li>
+            <li class="benifit-text">Online Banking</li>
+            <li class="benifit-text">Investment Report Every Month</li>
+            <li class="benifit-text">Saving Money For The Future</li>
+            <li class="benifit-text">Online Transection</li>
+          </ul>
+        </div>
+        <div>
+          <img class="benifit-img" src="./images/mac.png" alt="imgage Part 3" />
+          <div class="benifit-des1">
+            <div><img class="benifir-img-per" src="./images/person.png" alt="Icon Persion" /></div>
+            <div>
+              <p>Anmada Young</p>
+              <p>Expert Saving Money</p>
+            </div>
+            <div><img class="benifir-img-per" src="./images/person.png" alt="icon message" /></div>
+          </div>
+          <div class="benifit-des2">
+            <div><img class="benifir-img-per" src="./images/person.png" alt="Icon " /></div>
+            <p>Money Tranfer Successful</p>
+          </div>
+
+          <div class="benifit-des1">
+            <div>
+              <p>Total Income</p>
+              <p>$240</p>
+            </div>
+            <div><img class="benifir-img-per" src="./images/person.png" alt="Icon grouth up" /></div>
+          </div>
+        </div>
+      </section>
+
+      <!-- Part 4 -->
+      <section class="choose-plan-section">
+      <h2 class="section-title">Choose Plan</h2>
+      <h2 class="section-title">That's Right For You</h2>
+      <div class="billing-options">
+        <p class="billing-text">Choose plan that works best for you, feel free to contact us</p>
+        <div class="billing-buttons">
+          <button class="btn billing-btn" id="option1">Bill Monthly</button>
+          <button class="btn billing-btn" id="option2">Bill Yearly</button>
+        </div>
+      </div>
+      <div class="plan-cards">
+        <div class="plan-card">
+          <h3 class="plan-title">Free</h3>
+          <h4 class="plan-title">Have a go ...</h4>
+          <p class="plan-price">$0</p>
+          <div class="feature-container">
+            <div class="feature-box">
+              <ul class="feature-checklist">
+                <li class="completed">2 User</li>
+                <li>Subfeature 2</li>
+                <li>Subfeature 3</li>
+              </ul>
+            </div>
+          </div>
+          <button class="btn">Signup for free</button>
+        </div>
+        <div class="plan-card">
+          <h3 class="plan-title">Pro</h3>
+          <h4 class="plan-title">Have a go ...</h4>
+          <p class="plan-price">$19.99/month</p>
+          <div class="feature-container">
+            <div class="feature-box">
+              <ul class="feature-checklist">
+                <li class="completed">Subfeature 1</li>
+                <li>Subfeature 2</li>
+                <li>Subfeature 3</li>
+              </ul>
+            </div>
+          </div>
+          <button class="btn">Go to pro</button>
+        </div>
+        <div class="plan-card">
+          <h3 class="plan-title">Premium</h3>
+          <h4 class="plan-title">Have a go ...</h4>
+          <p class="plan-price">$29.99/month</p>
+          <div class="feature-container">
+            <div class="feature-box">
+              <ul class="feature-checklist">
+                <li class="completed">Subfeature 1</li>
+                <li>Subfeature 2</li>
+                <li>Subfeature 3</li>
+              </ul>
+            </div>
+          </div>
+          <button class="btn">Goto Business</button>
+        </div>
+      </div>
+      </section>
+
+
+      <!-- Part 5 -->
+      <section class="contact" id="contact">
+        <div>
+          <h1>People are Saying About DoWhith</h1>
+          <p>Everything you need to accept to payment and grow your money of manage anywhere on planet</p>
+          <div>//</div>
+
+          <p>I am very helped by this E-wallet application . my days are very easy to use this application..</p>
+          <p>_huyhq</p>
+
+          <div>
+            <a href=""><img src="" alt="Profile Image" /></a>
+            <a href=""><img src="" alt="Profile Image" /></a>
+            <a href=""><img src="" alt="Profile Image" /></a>
+            <a href=""><img src="" alt="Profile Image" /></a>
+            <a href=""><img src="" alt="Profile Image" /></a>
+          </div>
+        </div>
+
+        <div>
+          <div>
+            <img src="./images/mac.png" alt="Databricks" />
+            <h2 class="contact-text">Get Started</h2>
+          </div>
+
+          <div class="contact-info">
+            <form class="request-form" action="">
+              <div>
+                <label for="">Email</label>
+                <input type="email" placeholder="Enter your email" />
+              </div>
+
+              <div>
+                <label for="">Message</label>
+                <input type="message" placeholder="What are you say"> </input>
+              </div>
+              <div>
+                <button type="submit" class="btn-request">Request Demo</button>
+              </div>
+              <div>
+                <div class="start-free-trial">
+                  <span>or</span>
+                  <a href=""> Start Free Trial</a>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </section>
+    </main>
+    <!-- footer -->
+    <footer id="footer" class="footer">
+      <div class="footer-list">
+        <div class="footer-intro">
+          <div class="logo">
+            <p>Biccas</p>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas
+            ipsa neque alias, consequatur enim, repellendus provident
+            necessitatibus eaque sed totam iure eligendi ipsam architecto ullam
+            impedit. Quod inventore atque obcaecati?
+          </p>
+          <div>
+            <input type="text" placeholder="email" />
+            <img src="" alt="Icon" />
+          </div>
+        </div>
+        <nav class="footer-text">
+          <ul>
+            <li><a href="">Support</a></li>
+            <li><a href="">Home</a></li>
+            <li><a href="">Home</a></li>
+            <li><a href="">Home</a></li>
+            <li><a href="">Home</a></li>
+          </ul>
+        </nav>
+        <nav class="footer-text">
+          <ul>
+            <li><a href="">Help And Solution</a></li>
+            <li><a href="">Home</a></li>
+            <li><a href="">Home</a></li>
+            <li><a href="">Home</a></li>
+            <li><a href="">Home</a></li>
+          </ul>
+        </nav>
+        <nav class="footer-text">
+          <ul>
+            <li><a href="">Product</a></li>
+            <li><a href="">Home</a></li>
+            <li><a href="">Home</a></li>
+            <li><a href="">Home</a></li>
+            <li><a href="">Home</a></li>
+            <li><a href="">Home</a></li>
+          </ul>
+        </nav>
+      </div>
+      <div class="link-contact">
+        <p>Coppry right by <a href="fb.com/aada">@huyhq</a></p>
+        <div>
+          <a href="">Term</a>
+          <a href="">Policy</a>
+        </div>
+      </div>
+    </footer>
+  </body>
+</html>
